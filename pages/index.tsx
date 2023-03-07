6df4b0897cd3da9fe36e472338062d6a43d6ec6a
Jorge Ramos <jorgeramos2068@gmail.com>
2023-03-06 20:48:27 -0600

    Install NextUI and update _document.tsx file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import { NextPage } from 'next';
+import { Button } from '@nextui-org/react';
 
 const HomePage: NextPage = () => {
   return (
     <>
       <h1>Hello World</h1>
+      <Button color="gradient">Hello World</Button>
     </>
   );
 };
