d697b1096524403d9d430e006d12e07fa7068949
Jorge Ramos <jorgeramos2068@gmail.com>
2023-03-09 20:58:24 -0600

    Perform backend call in getStaticProps to create the list of pokemons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,46 @@
 import { GetStaticProps, NextPage } from 'next';
 import { MainLayout } from '@/components/layouts';
+import { pokeapi } from '@/api';
+import { PokemonListResponse, PokemonResult } from '@/interfaces';
 
-const HomePage: NextPage = props => {
-  console.log('Props:', props);
+interface Props {
+  pokemons: PokemonResult[];
+}
 
+const HomePage: NextPage<Props> = ({ pokemons }) => {
+  console.log('Poke 2:', pokemons);
   return (
     <MainLayout title="Pokemon App | Pokemon List">
       <h1>Pokemon App</h1>
+      <ul>
+        {pokemons.map(({ id, name }) => (
+          <li>
+            {id} - {name}
+          </li>
+        ))}
+      </ul>
     </MainLayout>
   );
 };
 
-export const getStaticProps: GetStaticProps = context => {
+export const getStaticProps: GetStaticProps = async context => {
+  const { data } = await pokeapi.get<PokemonListResponse>('/pokemon?limit=151');
+  const pokemons: PokemonResult[] = data.results.map(({ name, url }) => {
+    const splittedUrl = url.split('/');
+    const id = splittedUrl[splittedUrl.length - 2];
+    const img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`;
+    return {
+      name,
+      url,
+      id: parseInt(id) || 0,
+      img,
+    };
+  });
+
   return {
     // Will be passed to the page component as props
     props: {
-      name: 'Batman',
+      pokemons,
     },
   };
 };
